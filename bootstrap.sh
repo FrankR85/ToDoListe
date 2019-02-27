ba6df43eaad370679902bf6bb062db10aa86b2a5
Christian Garbs <mitch@cgarbs.de>
2019-02-27 20:25:14 +0100

    remove Apache configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,6 @@
 echo "Provisioning..."
 sudo apt-get update
 
-sudo apt-get install -y apache2 npm nodejs-legacy uwsgi
-
-cd /var/www/html
-sudo ln -s /vagrant/www www
-#todo-app
-sudo ln -s /vagrant/www/todo/index.html todo
-
-#enable CGI
-cd /etc/apache2/mods-enabled
-sudo ln -s ../mods-available/cgi.load
-
-cd /etc/apache2/conf-available
-sudo rm serve-cgi-bin.conf
-sudo ln -s /vagrant/apache/config/serve-cgi-bin.conf serve-cgi-bin.conf
-cd /usr/lib/cgi-bin/
-
-#set symlinks to executable cobol pgms
-sudo ln -s /vagrant/todoliste/target/TODOHANDLER todohandler
-
-touch todolist.txt
-sudo chmod 0777 todolist.txt
-sudo chown www-data:www-data todolist.txt
-chmod 0744 todolist.txt
-
-sudo service apache2 start
-
+sudo apt-get install -y npm nodejs-legacy uwsgi
 
 echo "cd /vagrant/cobol" >> /home/vagrant/.bashrc
